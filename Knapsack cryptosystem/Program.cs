7c51bd5a7eaad64f7e92d10d1f3413bddc716113
Admin <Admin@DESKTOP-SQI25PP>
2017-05-04 15:05:23 +0200

    Add project files.

---
+++ Knapsack cryptosystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knapsack_cryptosystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
